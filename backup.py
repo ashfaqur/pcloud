e4012921475753a5e715497fecdfd41e1ceefc89
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2024-11-26 15:03:49 +0100

    wip

--- backup.py
+++ backup.py
@@ -1,111 +1,93 @@
 import json
 import shutil
+import subprocess
 import os
-import datetime
+from datetime import datetime
 
-from pcloud import PyCloud  # type: ignore
+
+INPUT_FILE = "env/input.json"
+CLOUD_PATH = "cloudpath"
+LOCAL_PATH = "localpath"
 
 paths: list[str] = []
 
 
-def main() -> None:
-
-    input_file = "env/input.json"
-
+def validate_input() -> dict[str, str]:
+    if not os.path.exists(INPUT_FILE):
+        raise Exception(f"Input file '{INPUT_FILE}' does not exist.")
     # Open and read the JSON file
-    with open(input_file, "r") as file:
-        data = json.load(file)
-
-    username = data.get("username")
-    if not username:
-        raise Exception(
-            "The 'username' field does not exist or is empty in the JSON data."
-        )
-
-    password = data.get("password")
-    if not password:
-        raise Exception(
-            "The 'password' field does not exist or is empty in the JSON data."
-        )
-
-    endpoint = data.get("endpoint")
-    if not endpoint:
-        raise Exception(
-            "The 'endpoint' field does not exist or is empty in the JSON data."
-        )
-
-    cloudpath = data.get("cloudpath")
+    with open(INPUT_FILE, "r") as file:
+        data: dict[str, str] = json.load(file)
+    cloudpath = data.get(CLOUD_PATH)
     if not cloudpath:
-        raise Exception(
-            "The 'cloudpath' field does not exist or is empty in the JSON data."
-        )
-    localpath = data.get("localpath")
+        raise Exception("The 'cloudpath' field does not exist.")
+    if not os.path.exists(cloudpath):
+        raise Exception(f"The cloudpath '{cloudpath}' does not exist")
+
+    localpath = data.get(LOCAL_PATH)
     if not localpath:
-        raise Exception(
-            "The 'localpath' field does not exist or is empty in the JSON data."
+        raise Exception("The 'localpath' field does not exist.")
+    if not os.path.exists(localpath):
+        raise Exception(f"The localpath '{localpath}' does not exist")
+
+    return data
+
+
+def main(input: dict[str, str]) -> None:
+    localPath = input[LOCAL_PATH]
+    cloudPath: str = input[CLOUD_PATH]
+    files_skipped = 0
+    files_copied = 0
+    # Walk through the directory tree
+    for root, dirs, files in os.walk(cloudPath):
+        for file_name in files:
+            absolute_path = os.path.join(root, file_name)
+            relative_path = os.path.relpath(absolute_path, cloudPath)
+            destination_path = os.path.join(localPath, relative_path)
+            # print(f"Processing file: {file_name}")
+            # print(f"  Source: {absolute_path}")
+            # print(f"  Destination: {destination_path}")
+            # Ensure the destination directory exists
+            destination_dir = os.path.dirname(destination_path)
+            if not os.path.exists(destination_dir):
+                # print(f"Creating directory: {destination_dir}")
+                os.makedirs(destination_dir)
+            if not os.path.exists(destination_path):
+                # print(f"Copying to {localPath}")
+                files_copied += 1
+                # shutil.copyfile(absolute_path, os.path.join(localPath, relative_path))
+            else:
+                files_skipped += 1
+                # print(f"File already exists in {localPath}")
+    print("Summary:")
+    print(f"Files copied: {files_copied}")
+    print(f"Files skipped: {files_skipped}")
+
+    run_git_commands(localPath, "update")
+
+
+def run_git_commands(git_repo: str, commit_message: str):
+    try:
+        os.chdir(git_repo)
+        result = subprocess.run(
+            ["git", "status", "--porcelain"],
+            check=True,
+            stdout=subprocess.PIPE,
+            text=True,
         )
-
-    modification_date = get_folder_modification_date(localpath)
-
-    print(f"The folder was last modified on: {modification_date}")
-
-    print(f"Backuping {cloudpath} to {localpath}")
-
-    pc = PyCloud(username, password, endpoint)
-    info = pc.listfolder(path="/Documents", recursive=True)
-
-    contents = info.get("metadata").get("contents")
-    print_contents(contents, "/Documents")
-
-    file_path = "build/output.json"
-
-    # Writing the JSON data to a file
-    with open(file_path, "w") as file:
-        json.dump(info, file, indent=4)
-
-    print(f"JSON data has been saved to {file_path}")
-
-    for path in paths:
-        # print(f"Path is {path}")
-        localFilePath = path.replace("/Documents/", localpath)
-        # print(f"Local file path is {localFilePath}")
-
-        if not os.path.exists(localFilePath):
-            # print(f"File does not exist: {localFilePath}")
-            directory = os.path.dirname(localFilePath)
-            if not os.path.exists(directory):
-                print(f"Creating directory: {directory}")
-                os.makedirs(directory)
-            newPath = path.replace("/Documents/", "/home/ash/pCloudDrive/Documents/")
-            print(newPath)
-            print(localFilePath)
-            shutil.copyfile(newPath, localFilePath)
-            # print(f"Result: {code}")
-
-
-def print_contents(contents, parent):
-    for item in contents:
-        if item.get("isfolder"):
-            path = parent + "/" + item.get("name")
-
-            # Recursively call the function to handle the nested contents
-            if item.get("contents"):
-                print_contents(item.get("contents"), path)
-        else:
-            path = parent + "/" + item.get("name")
-            paths.append(path)
-            # print(f"File: {path}")
-
-
-def get_folder_modification_date(folder_path):
-    # Get the last modification time (in seconds since the epoch)
-    modification_time = os.path.getmtime(folder_path)
-
-    # Convert the modification time to a human-readable format
-    modification_date = datetime.datetime.fromtimestamp(modification_time)
-
-    return modification_date
+        if not result.stdout.strip():
+            print("No changes to commit. Working tree is clean.")
+            return
+        subprocess.run(["git", "add", "."], check=True)
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error while running Git command: {e}")
+    except FileNotFoundError:
+        print("Git is not installed or the path is incorrect.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    input: dict[str, str] = validate_input()
+    main(input)
